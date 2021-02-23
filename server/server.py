ff999c08fc3faea74bfbda5515d2657abe4ca55d
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2021-02-23 15:29:09 -0300

    refactor: melhorando server

--- server/server.py
+++ server/server.py
@@ -1,25 +1,16 @@
+# Echo server program
 import socket
 
-
-HOST = 'localhost'
-PORT = 40000
-
-# INET - trabalhar com IPv4, SOCK_STREAM - TCP
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))     # Vincula o host e a porta com o socket
-server.listen()
-print("Aguardando conexão de um cliente")
-
-# Aceita a conexão com o cliente e retorna a conexão e o endereço do cliente
-conn, address = server.accept()
-print(f"Conectado em {address}")
-
-while True:
-    data = conn.recv(1024)  # Recebe os dados do cliente
-
-    if not data:
-        print("Fechando a conexão")
-        conn.close()    # Finaliza a conexão
-        break
-
-    conn.sendall(data)  # Envia dados para o cliente
+HOST = 'localhost'                 # Symbolic name meaning all available interfaces
+PORT = 40000              # Arbitrary non-privileged port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    print("Aguardando conexão de um cliente")
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data)
