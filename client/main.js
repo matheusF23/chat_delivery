b847aab9f09a26d46ff12a57f8042309d96fa15a
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2021-02-25 14:18:07 -0300

    feat: implementacao do cliente

---
+++ client/main.js
@@ -0,0 +1,31 @@
+
+
+var ws = new WebSocket("ws://127.0.0.1:8765/")
+
+document.addEventListener('keypress', (e) => {
+    if (e.key == "Enter") {
+        newMessage();
+    }
+});
+
+function newMessage() {
+    var message = document.createElement("li");
+    var messageValue = document.getElementById("message").value;
+    var content = document.createTextNode(messageValue);
+    message.appendChild(content);
+    if (messageValue === '') {
+        alert("Você precisa digitar alguma coisa!");
+    } else {
+        document.getElementById("chatUL").appendChild(message);
+        ws.send(messageValue.toLowerCase());
+    }
+    document.getElementById("message").value = "";
+}
+
+ws.onmessage = function (event) {
+    var message = document.createElement("li");
+    message.className = 'botLI'
+    var content = document.createTextNode(event.data);
+    message.innerHTML = event.data;
+    document.getElementById("chatUL").appendChild(message);
+};
